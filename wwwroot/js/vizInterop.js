7bfcaa9b7257b8fa4ee5637fd0e78392bc8c9814
Olivier Duhart <olivier.duhart@sage.com>
2024-06-12 11:21:51 +0200

    viz component initial

--- wwwroot/js/vizInterop.js
+++ wwwroot/js/vizInterop.js
@@ -15,6 +15,23 @@
 
 }
 
+function vizRenderOn(graphvizText,target) {
+    var viz = new Viz();
+
+    //viz.renderSVGElement(graphvizText)
+    viz.renderSVGElement(graphvizText)
+        .then(function (element) {
+            updateVizWithPanZoom(element, target);
+        })
+        .catch(error => {
+            // Create a new Viz instance (@see Caveats page for more info)
+            viz = new Viz();
+
+            appendError(error);
+        });
+
+}
+
 
 
 function cleanGraph(graph) {
@@ -33,7 +50,37 @@ function cleanGraph(graph) {
 
 function updateVizWithPanZoom(svg) {
 
-    var graph = document.querySelector("#output");
+    return updateVizWithPanZoomOn("#output");
+    // var graph = document.querySelector("#output");
+    //
+    // cleanGraph(graph);
+    //
+    // svg.id = 'svg';
+    // graph.appendChild(svg);
+    //
+    //
+    // panZoom = svgPanZoom(svg, {
+    //     zoomEnabled: true,
+    //     controlIconsEnabled: true,
+    //     fit: true,
+    //     center: true,
+    //     minZoom: 0.1
+    // });
+    //
+    //
+    // svg.addEventListener('paneresize', function (e) {
+    //     panZoom.resize();
+    // }, false);
+    // window.addEventListener('resize', function (e) {
+    //     panZoom.resize();
+    // });
+    //
+    // return svg
+}
+
+function updateVizWithPanZoomOn(svg, target) {
+
+    var graph = document.querySelector(target);
 
     cleanGraph(graph);
 
@@ -66,4 +113,14 @@ function cleanViz() {
     cleanGraph(graph);
 }
 
+
+function buildSvg() {
+    import { instance } from "@viz-js/viz";
+
+    instance().then(viz => {
+        const svg = viz.renderSVGElement("digraph { a -> b }");
+
+        document.getElementById("graph").appendChild(svg);
+    });
+}
 
