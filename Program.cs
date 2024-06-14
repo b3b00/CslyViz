e7af0d950af85e18f33560e8b3815d2b3ca8c19e
Olivier Duhart <olivier.duhart@sage.com>
2024-06-14 15:24:30 +0200

    csly-cli-api 1.3.7

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using csly_cli_api;
 using CslyViz;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -9,9 +10,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddSingleton<ICslyContext, CslyContext>();
+builder.Services.AddSingleton<ICslyProcessor, CslyProcessor>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton(serviceProvider => (IJSInProcessRuntime)serviceProvider.GetRequiredService<IJSRuntime>());
 builder.Services.AddSingleton(serviceProvider => (IJSUnmarshalledRuntime)serviceProvider.GetRequiredService<IJSRuntime>());
+
 
 builder.Services.AddBlazorDialog();
 
