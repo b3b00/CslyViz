60867f84dd9b50f6729f81883ea304d0bfba4ee2
b3b00 <1224790+b3b00@users.noreply.github.com>
2025-03-21 14:42:17 +0000

    Deploying to gh-pages from  @ 016bc09c3bb9ea83941d01adadaf5ef987278012 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -109,7 +109,7 @@
       "url": "_framework\/icudt_no_CJK.dat"
     },
     {
-      "hash": "sha256-Zzy7C5thl4pflIDyPd1cknTNMxwlmgkdlgPNnve+\/Ss=",
+      "hash": "sha256-Qhy8Sm6ip+zzL9Xja3vz9nZGsk5Wr2WIZOKHiHmuuoI=",
       "url": "_framework\/blazor.boot.json"
     },
     {
@@ -137,7 +137,7 @@
       "url": "_framework\/csly-cli-parser.wasm"
     },
     {
-      "hash": "sha256-OahppJWDScqr9oP+T9SZoX6yPgwjLWio7PQI+8TqKd8=",
+      "hash": "sha256-KIunDZFy9ST6GvV9sYPiwiI2CAaoqhCjPXlLzI6mOSM=",
       "url": "_framework\/CslyViz.wasm"
     },
     {
@@ -565,5 +565,5 @@
       "url": "_content\/BlazorDialog\/styles.min.css"
     }
   ],
-  "version": "fe\/Qx9zn"
+  "version": "8EdczsUp"
 };
--- service-worker.js
+++ service-worker.js
@@ -53,4 +53,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Manifest version: fe/Qx9zn */
+/* Manifest version: 8EdczsUp */
